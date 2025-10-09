1bd7fd33129f82edeacca6861357af22060746c9
RepBit <nguyen.dh.khoa@gmail.com>
2025-10-08 21:11:59 -0400

    Add files via upload

---
+++ Lab5_PWM_threadedCallbacks.py
@@ -0,0 +1,23 @@
+#import RPi.GPIO as GPIO
+import math
+import time
+
+#GPIO.setmode(GPIO.BCM)
+
+p = 23
+f = 0.2 # Hz
+t = 1
+B = math.pow(math.sin(2*math.pi*f*t),2)
+
+GPIO.setup(p, GPIO.OUT)
+pwm = GPIO.PWM(p, 500)
+
+try:
+  pwm.start(B)
+  while true:
+    pass
+  except KeyboardInterrupt:
+    print("\nExiting")
+
+pwm.stop()
+GPIO.cleanup()
