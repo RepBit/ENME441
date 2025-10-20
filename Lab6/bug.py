678ee2eab4bd04673e2c692f6824c31dab631736
RepBit <nguyen.dh.khoa@gmail.com>
2025-10-20 15:07:31 -0400

    Update bug.py

--- Lab6/bug.py
+++ Lab6/bug.py
@@ -1,3 +1,60 @@
+import time
+import random
+import RPi.GPIO as GPIO
+from shifter import Shifter
+
+class Bug:
+    def __init__(self, serialPin, clockPin, latchPin, timestep=0.1, x=3, isWrapOn=False):
+        self.timestep = timestep
+        self.x = x
+        self.isWrapOn = isWrapOn
+        self._shifter = Shifter(serialPin, clockPin, latchPin)
+        self.running = False
+        self.led_array = [1, 2, 4, 8, 16, 32, 64, 128]
+
+    def _display(self):
+        self._shifter.shiftByte(self.led_array[self.x])
+
+    def _clear(self):
+        self._shifter.shiftByte(0)
+
+    def _move(self):
+        step = random.choice([-1, 1])
+        new_x = self.x + step
+        if self.isWrapOn:
+            new_x %= 8
+        else:
+            new_x = max(0, min(7, new_x))
+        self.x = new_x
+
+    def step(self):
+        """Perform a single movement step if running."""
+        if self.running:
+            self._display()
+            self._move()
+
+    def clear(self):
+        """Clear the LEDs."""
+        self._clear()
+
+    def start(self):
+        """Start continuous movement (blocking)."""
+        self.running = True
+        try:
+            while self.running:
+                self.step()
+                time.sleep(self.timestep)
+        except KeyboardInterrupt:
+            self.stop()
+
+    def stop(self):
+        self.running = False
+        self._clear()
+        GPIO.cleanup()
+        print("\nBug stopped and GPIO cleaned up.")
+Updated bug.py (polls switches, no threading)
+python
+Copy code
 import RPi.GPIO as GPIO
 import time
 from shifter import Bug
@@ -19,7 +76,6 @@ GPIO.setup(s3_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 # --------------------------
 bug = Bug(serialPin=23, clockPin=24, latchPin=25, timestep=0.1)
 
-# Track previous state of s2 to detect changes
 prev_s2_state = GPIO.input(s2_pin)
 
 try:
@@ -29,25 +85,23 @@ try:
         s2 = GPIO.input(s2_pin)
         s3 = GPIO.input(s3_pin)
 
-        # --- Control bug start/stop ---
-        if s1 and not bug._Bug__running:
-            bug._Bug__running = True
-        elif not s1 and bug._Bug__running:
-            bug._Bug__running = False
-            bug._Bug__clear()
+        # --- Start/stop Bug ---
+        if s1:
+            bug.running = True
+        else:
+            bug.running = False
+            bug.clear()
 
-        # --- Toggle wrap mode when s2 changes (rising edge) ---
+        # --- Toggle wrap mode on rising edge of s2 ---
         if s2 != prev_s2_state and s2 == 1:
             bug.isWrapOn = not bug.isWrapOn
         prev_s2_state = s2
 
-        # --- Adjust speed when s3 is on ---
-        bug.timestep = 0.033 if s3 else 0.1  # 3x faster if s3 pressed
+        # --- Adjust speed ---
+        bug.timestep = 0.033 if s3 else 0.1  # 3x faster when s3 pressed
 
         # --- Step Bug if running ---
-        if bug._Bug__running:
-            bug._Bug__display()
-            bug._Bug__move()
+        bug.step()
 
         time.sleep(bug.timestep)
 
