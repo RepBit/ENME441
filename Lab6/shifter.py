307c01b150fe1a11b974eb191759d61383374f9a
RepBit <nguyen.dh.khoa@gmail.com>
2025-10-19 23:17:06 -0400

    Update shifter.py

--- Lab6/shifter.py
+++ Lab6/shifter.py
@@ -1,27 +1,41 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
+class Shifter:
+  def __init__(self, serialPin, clockPin, latchPin):
+    self.serialPin = serialPin
+    self.clockPin = clockPin
+    self.latchPin = latchPin
 
-dataPin, latchPin, clockPin = 23, 24, 25
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(self.serialPin, GPIO.OUT)
+    GPIO.setup(self.clockPin, GPIO.OUT, initial=0)
+    GPIO.setup(self.latchPin, GPIO.OUT, initial=0)
+  
+  def __ping(self, pin): #_private
+    GPIO.output(pin, 1)
+    time.sleep(0)
+    GPIO.output(pin, 0)
 
-GPIO.setup(dataPin, GPIO.OUT)
-GPIO.setup(latchPin, GPIO.OUT, initial=0)  # start latch & clock low
-GPIO.setup(clockPin, GPIO.OUT, initial=0)  
+  def shiftByte(self, b): #public
+    for i in range(8):
+      GPIO.output(self.serialPin, b & (1 << i))
+      self.__ping(self.clockPin)    # add bit to register
+    self.__ping(self.latchPin)      # send register to output
 
-pattern = 0b01100110        # 8-bit pattern to display on LED bar
 
-for i in range(8):
-  GPIO.output(dataPin, pattern & (1<<i))
-  GPIO.output(clockPin,1) 	    # ping the clock pin to shift register data
-  time.sleep(0)
-  GPIO.output(clockPin,0)
+if __name__ == "__main__":
+  try:
+    shifter = Shifter(serialPin=23, clockPin=25, latchPin=24)
+    pattern = 0b01100110
 
-GPIO.output(latchPin, 1)        # ping the latch pin to send register to output
-time.sleep(0)
-GPIO.output(latchPin, 0)
+    shifter.shiftByte(pattern)
+    
+    while True:
+            pass
+  
+  except KeyboardInterrupt:
+        GPIO.cleanup()
 
-try:
-  while 1: pass
-except:
-  GPIO.cleanup()
+
+
