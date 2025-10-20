e5b77c3f5cf28b705808fe6b2037a5141decb5df
RepBit <nguyen.dh.khoa@gmail.com>
2025-10-20 00:36:07 -0400

    Update bug.py

--- Lab6/bug.py
+++ Lab6/bug.py
@@ -1,63 +1,58 @@
 import RPi.GPIO as GPIO
 import time
-import Bug
+from shifter import Bug
 
-# --- GPIO Setup ---
+# --------------------------
+# GPIO input pins
+# --------------------------
+s1_pin = 17   # Switch to start/stop the bug
+s2_pin = 27   # Switch to toggle wrap mode
+s3_pin = 22  # Switch to increase speed (reduce delay)
+
 GPIO.setmode(GPIO.BCM)
-s1, s2, s3 = 5, 6, 13  # example input pins, change as needed
+GPIO.setup(s1_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(s2_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(s3_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.setup(s1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(s2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(s3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+# --------------------------
+# Instantiate Bug
+# --------------------------
+bug = Bug(serialPin=23, clockPin=24, latchPin=25, timestep=0.1)
 
-# --- Instantiate Bug ---
-bug = Bug(serialPin=23, clockPin=25, latchPin=24)  # uses default timestep=0.1, x=3, isWrapOn=False
-
-# --- State Tracking ---
-prev_s2_state = GPIO.input(s2)
-bug_running = False
-
-print("Bug control started:")
-print(" s1: On/Off | s2: Toggle wrap | s3: Speed x3\nPress Ctrl+C to exit.")
+# Track previous state of s2 to detect changes
+prev_s2_state = GPIO.input(s2_pin)
 
 try:
     while True:
         # --- Read switch states ---
-        s1_state = GPIO.input(s1)
-        s2_state = GPIO.input(s2)
-        s3_state = GPIO.input(s3)
+        s1 = GPIO.input(s1_pin)
+        s2 = GPIO.input(s2_pin)
+        s3 = GPIO.input(s3_pin)
 
-        # (a) s1 → Turn Bug on/off
-        if s1_state and not bug_running:
-            bug_running = True
-            print("Bug ON")
-        elif not s1_state and bug_running:
-            bug_running = False
-            bug.stop()
-            print("Bug OFF")
+        # --- Control bug start/stop ---
+        if s1 and not bug._Bug__running:  # private attribute access
+            bug._Bug__running = True
+        elif not s1 and bug._Bug__running:
+            bug._Bug__running = False
+            bug._Bug__clear()
 
-        # (b) s2 → Flip wrapping when state changes
-        if s2_state != prev_s2_state:
-            if s2_state:  # on rising edge
-                bug.isWrapOn = not bug.isWrapOn
-                print(f"Wrap mode {'ON' if bug.isWrapOn else 'OFF'}")
-            prev_s2_state = s2_state
+        # --- Toggle wrap mode when s2 changes ---
+        if s2 != prev_s2_state:
+            bug.isWrapOn = not bug.isWrapOn
+            prev_s2_state = s2
 
-        # (c) s3 → Adjust speed (3x faster when pressed)
-        if s3_state:
-            bug.timestep = 0.1 / 3
+        # --- Adjust speed when s3 is on ---
+        if s3:
+            bug.timestep = max(0.01, 0.1 / 3)  # increase speed by 3x
         else:
-            bug.timestep = 0.1
+            bug.timestep = 0.1  # default speed
 
-        # --- Move the bug if active ---
-        if bug_running:
-            bug._Bug__display()  # use Bug’s private display method to show position
-            time.sleep(bug.timestep)
-            bug._Bug__move()     # random step
-        else:
-            time.sleep(0.05)
+        # --- If bug is running, move & display ---
+        if bug._Bug__running:
+            bug._Bug__display()
+            bug._Bug__move()
+
+        time.sleep(bug.timestep)
 
 except KeyboardInterrupt:
     bug.stop()
-    GPIO.cleanup()
-    print("\nProgram stopped and GPIO cleaned up.")
