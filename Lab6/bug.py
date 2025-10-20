5400a2fa01be62b9a92e0610d1eb5778417901f1
RepBit <nguyen.dh.khoa@gmail.com>
2025-10-20 15:06:06 -0400

    Update bug.py

--- Lab6/bug.py
+++ Lab6/bug.py
@@ -7,7 +7,7 @@ from shifter import Bug
 # --------------------------
 s1_pin = 17   # Switch to start/stop the bug
 s2_pin = 27   # Switch to toggle wrap mode
-s3_pin = 22  # Switch to increase speed (reduce delay)
+s3_pin = 22   # Switch to increase speed (reduce delay)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(s1_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -30,24 +30,21 @@ try:
         s3 = GPIO.input(s3_pin)
 
         # --- Control bug start/stop ---
-        if s1 and not bug._Bug__running:  # private attribute access
+        if s1 and not bug._Bug__running:
             bug._Bug__running = True
         elif not s1 and bug._Bug__running:
             bug._Bug__running = False
             bug._Bug__clear()
 
-        # --- Toggle wrap mode when s2 changes ---
-        if s2 != prev_s2_state:
+        # --- Toggle wrap mode when s2 changes (rising edge) ---
+        if s2 != prev_s2_state and s2 == 1:
             bug.isWrapOn = not bug.isWrapOn
-            prev_s2_state = s2
+        prev_s2_state = s2
 
         # --- Adjust speed when s3 is on ---
-        if s3:
-            bug.timestep = max(0.01, 0.1 / 3)  # increase speed by 3x
-        else:
-            bug.timestep = 0.1  # default speed
+        bug.timestep = 0.033 if s3 else 0.1  # 3x faster if s3 pressed
 
-        # --- If bug is running, move & display ---
+        # --- Step Bug if running ---
         if bug._Bug__running:
             bug._Bug__display()
             bug._Bug__move()
